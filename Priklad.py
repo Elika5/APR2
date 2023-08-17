91843be5491045f2c4e9983f82595be01151d5d6
PetrHejkal <petrhejkal@gmail.com>
2023-08-17 22:10:05 +0200

    update

--- Priklad.py
+++ Priklad.py
@@ -5,15 +5,21 @@ import numpy as np
 def generujPriklad(databaze):
     typPrikladu = random.randint(1,3)
     if typPrikladu==1:
-        A=random.randint
-        B=random.randint
-        C=random.randint
+         nezname = {
+    "[neznama1]": random.randint,
+    "[neznama2]": random.randint,
+    "[neznama3]": random.randint,
+}
+
+    text="Vypočítejte rovnici [neznama1]x² + [neznama2]x + [neznama3] = 0"
+    for neznama, nezname in nezname.items():
+        zkouškaNovehoTextu = text.replace("[" + neznama + "]",nezname)
 
     elif typPrikladu==2:
-        pocet_cisel=random.randint(4,10)
+        pocet_cisel=random.randint(4,12)
         cislaPriklad=[]
         for i in range(pocet_cisel):
-            cislaPriklad.append(random.randint)
+            cislaPriklad.append(random.randint(1,3))
 
     else:
         nahodna_matice = np.random.rand(3, 3)
