ce6359eede22d085b7db06f1a188333c63b20236
Elika5 <eli.vagner3@gmail.com>
2023-08-18 10:45:00 +0200

    zkouším, kdyby 2 testy, výběr testu

--- databaze.py
+++ databaze.py
@@ -1,9 +1,18 @@
 class Databaze:
-      def __init__(self,soubor):
+      def __init__(self,soubor,typ_testu):
             self.soubor = soubor
+            self.typ_testu = typ_testu
 
       def otevreni_souboru(self):
             with open(self.soubor, "r") as soubor:
                   radky = soubor.readlines()
                   return radky
+      
+      def vyber_prikladu(self):
+            radky = self.otevreni_souboru()
+            if self.typ_testu == 1:
+                  return radky[1:5]
+            else:
+                  return radky[6:10]
+            
       
