1ba2fd1cd2ced80005da63641b0e6b81dd82b002
Elika5 <eli.vagner3@gmail.com>
2023-08-30 19:00:58 +0200

    tridy

--- Priklad.py
+++ Priklad.py
@@ -1,28 +1,6 @@
-import databaze
-import random
-import numpy as np
 
-def generujPriklad(databaze):
-    typPrikladu = random.randint(1,3)
-    if typPrikladu==1:
-         nezname = {
-    "[neznama1]": random.randint,
-    "[neznama2]": random.randint,
-    "[neznama3]": random.randint,
-}
+class Priklad:
 
-    text="Vypočítejte rovnici [neznama1]x² + [neznama2]x + [neznama3] = 0"
-    for neznama, nezname in nezname.items():
-        zkouškaNovehoTextu = text.replace("[" + neznama + "]",nezname)
-
-    elif typPrikladu==2:
-        pocet_cisel=random.randint(4,12)
-        cislaPriklad=[]
-        for i in range(pocet_cisel):
-            cislaPriklad.append(random.randint(1,3))
-
-    else:
-        nahodna_matice = np.random.rand(3, 3)
-
-
-
+    def __init__(self,zadani,vysledek):
+        self.zadani = zadani
+        self.vysledek = vysledek
---
+++ Zak.py
@@ -0,0 +1,6 @@
+
+class Zak:
+    def _init__(self,jmeno,znamky,soubor_studenta):
+        self.jmeno = jmeno
+        self.znamky = znamky
+        self.soubor_studenta = soubor_studenta
