e49f9bf54c6c0fd7e1b90f2734332ef182d40625
Elika5 <eli.vagner3@gmail.com>
2023-08-31 19:42:08 +0200

    soubor zaaka

--- Zak.py
+++ Zak.py
@@ -1,6 +1,22 @@
 
 class Zak:
-    def _init__(self,jmeno,znamky,soubor_studenta):
+    def _init__(self,jmeno,prijmeni,znamky):
         self.jmeno = jmeno
+        self.prijmeni = prijmeni
         self.znamky = znamky
-        self.soubor_studenta = soubor_studenta
+        self.soubor_nazev = f"{jmeno}_{prijmeni}.txt"
+
+    def hodnoceni(self,znamka):
+        self.znamky.append(znamka)
+
+    def otevri_nebo_vytvor_soubor(self):
+        try:
+            with open(self.soubor_nazev, "r", encoding="UTF-8") as soubor:
+                obsah = soubor.read()
+                print(f"Obsah souboru {self.soubor_nazev}:")
+                print(obsah)
+        except FileNotFoundError:
+            print(f"Soubor {self.soubor_nazev} nebyl nalezen. Vytvářím nový soubor...")
+            with open(self.soubor_nazev, "w", encoding="UTF-8") as soubor:
+                print(f"Nový soubor {self.soubor_nazev} byl vytvořen.")
+    
